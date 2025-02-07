641a131fe93b0662ca69b25979a794f281a3f69d
OstapchukAB <electron.anton@gmail.com>
2025-02-07 21:23:58 +0300

    рассмотрен шаблон абстрактная фабрика

---
+++ AbstractFabric/Class1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace WithoutAbstractFactory
+{
+    // Класс, отвечающий за создание кнопки для Windows
+    public class WinButton
+    {
+        public void Paint()
+        {
+            Console.WriteLine("Отрисовка кнопки Windows.");
+        }
+    }
+
+    // Класс, отвечающий за создание флажка для Windows
+    public class WinCheckbox
+    {
+        public void Paint()
+        {
+            Console.WriteLine("Отрисовка флажка Windows.");
+        }
+    }
+
+
+}
+
+
+namespace AbstractFactoryExample
+{
+    // Абстракции для компонентов
+    public interface IButton
+    {
+        void Paint();
+    }
+
+    public interface ICheckbox
+    {
+        void Paint();
+    }
+
+    // Абстрактная фабрика для создания элементов графического интерфейса
+    public interface IGUIFactory
+    {
+        IButton CreateButton();
+        ICheckbox CreateCheckbox();
+    }
+
+    // Конкретные реализации для Windows
+    public class WindowsButton : IButton
+    {
+        public void Paint()
+        {
+            Console.WriteLine("Отрисовка кнопки Windows (Abstract Factory).");
+        }
+    }
+
+    public class WindowsCheckbox : ICheckbox
+    {
+        public void Paint()
+        {
+            Console.WriteLine("Отрисовка флажка Windows (Abstract Factory).");
+        }
+    }
+
+    // Конкретная фабрика для Windows
+    public class WindowsFactory : IGUIFactory
+    {
+        public IButton CreateButton()
+        {
+            return new WindowsButton();
+        }
+
+        public ICheckbox CreateCheckbox()
+        {
+            return new WindowsCheckbox();
+        }
+    }
+
+   
+}
+
---
+++ UIAbstractFabric/Program.cs
@@ -0,0 +1,32 @@
+﻿using AbstractFactoryExample;
+
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        // Жёсткая привязка к конкретным реализациям (Windows)
+//        WinButton button = new WinButton();
+//        WinCheckbox checkbox = new WinCheckbox();
+
+//        button.Paint();
+//        checkbox.Paint();
+
+//        Console.ReadKey();
+//    }
+//}
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Клиентский код работает с абстракцией фабрики
+        IGUIFactory guiFactory = new WindowsFactory();
+
+        IButton button = guiFactory.CreateButton();
+        ICheckbox checkbox = guiFactory.CreateCheckbox();
+
+        button.Paint();
+        checkbox.Paint();
+
+        Console.ReadKey();
+    }
+}
