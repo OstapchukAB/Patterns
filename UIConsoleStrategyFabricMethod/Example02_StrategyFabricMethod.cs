e871da28b24b5ba8011a0c9ce198da7ad946ed03
OstapchukAB <electron.anton@gmail.com>
2025-02-13 13:07:01 +0300

    модифицирование кода + логирование реализованное через декоратор

--- UIConsoleStrategyFabricMethod/Example02_StrategyFabricMethod.cs
+++ UIConsoleStrategyFabricMethod/Example02_StrategyFabricMethod.cs
@@ -1,5 +1,4 @@
-﻿namespace TextTransformationAntiPattern;
-#region Начальные условия
+﻿#region Начальные условия
 /*
 Тема: Преобразование текста
 
@@ -48,108 +47,150 @@
         Выведи результат в консоль.
  */
 #endregion
-#region Стратегии обработки строки
-public interface ITextTransformStrategy
-{
-	public string TransformText(string text);
-}
-public class TextToUpperStrategy:ITextTransformStrategy
-{
-	public string TransformText(string text)=>text.ToUpper();
-}
-public class TextToLowerStrategy:ITextTransformStrategy
-{
-	public string TransformText(string text)=>text.ToLower();
-}
-public class TextToReverseStrategy:ITextTransformStrategy
-{
-	public string TransformText(string text)
-	{
-		 char[] arr = text.ToCharArray();
-         Array.Reverse(arr);
-         return new string(arr);
-	}
-}
+using System;
+using System.IO;
+using System.Linq;
+using System.Text;
 
-public class TextContext
+namespace TextTransformationExample
 {
-    ITextTransformStrategy _strategy;
-	public TextContext(ITextTransformStrategy strategy)
-	{
-		_strategy=strategy;
-	}
-	public void SetStrategy(ITextTransformStrategy strategy)
-	{
-		_strategy=strategy;
-	}
-	public string ExecuteAlgoritm(string text)
-	{
-		return _strategy.TransformText(text);
-	}
-}
-#endregion
+    #region Стратегии
 
-#region Фабрика стратегий
-abstract class TextStrategy
-{
-    public abstract ITextTransformStrategy GetStrategy();
-}
-class TextToUpper : TextStrategy
-{
-    public override ITextTransformStrategy GetStrategy()=> new TextToUpperStrategy();
-}
-class TextToLower : TextStrategy
-{
-    public override ITextTransformStrategy GetStrategy()=> new TextToLowerStrategy();
-
-}
-class TextToReverse : TextStrategy
-{
-    public override ITextTransformStrategy GetStrategy()=> new TextToReverseStrategy();
-}
-abstract class CreatorStrategy
-{
-	public abstract TextStrategy FactoryMethod();
-}
-class CreatorTextToUpper : CreatorStrategy
-{
-    public override TextStrategy FactoryMethod() => new TextToUpper();
-}
-class CreatorTextToLower :CreatorStrategy
-{
-    public override TextStrategy FactoryMethod() => new TextToLower();
-}
-class CreatorTextToReverse :CreatorStrategy
-{
-	public override TextStrategy FactoryMethod()=>new TextToReverse();
-}
-#endregion
-
-class Program
-{
-     static void Main(string[] args)
+    /// <summary>
+    /// Интерфейс для стратегии преобразования текста.
+    /// </summary>
+    public interface ITextTransformStrategy
     {
-        Console.WriteLine("Введите строку для преобразования:");
-        string input = Console.ReadLine();
+        string TransformText(string text);
+    }
 
-        Console.WriteLine("Выберите тип преобразования (upper, lower, reverse):");
-        string type = Console.ReadLine();
-		
-		
-        TextStrategy? factory = type switch 
-        { 
-            "upper" => new CreatorTextToUpper().FactoryMethod(),
-            "lower" => new CreatorTextToLower().FactoryMethod(),
-            "reverse" => new CreatorTextToReverse().FactoryMethod(),
-            _=> null
-        };
-        if (factory == null)
+    public class TextToUpperStrategy : ITextTransformStrategy
+    {
+        public string TransformText(string text) => text.ToUpper();
+    }
+
+    public class TextToLowerStrategy : ITextTransformStrategy
+    {
+        public string TransformText(string text) => text.ToLower();
+    }
+
+    public class TextToReverseStrategy : ITextTransformStrategy
+    {
+        public string TransformText(string text)
         {
-            Console.WriteLine("данная стратегия не рализована!");
-            return;
+            char[] arr = text.ToCharArray();
+            Array.Reverse(arr);
+            return new string(arr);
         }
-        var strategy = factory.GetStrategy();
-        var textContext= new TextContext(strategy);
-        Console.WriteLine("Результат: " + textContext.ExecuteAlgoritm(input));
+    }
+
+    /// <summary>
+    /// Декоратор для добавления логирования к стратегии преобразования.
+    /// </summary>
+    public class LoggingTextTransformStrategyDecorator : ITextTransformStrategy
+    {
+        private readonly ITextTransformStrategy _innerStrategy;
+
+        public LoggingTextTransformStrategyDecorator(ITextTransformStrategy innerStrategy)
+        {
+            _innerStrategy = innerStrategy;
+        }
+
+        public string TransformText(string text)
+        {
+            Console.WriteLine("Лог: Начало преобразования.");
+            string result = _innerStrategy.TransformText(text);
+            Console.WriteLine("Лог: Завершено преобразование.");
+            return result;
+        }
+    }
+
+    /// <summary>
+    /// Контекст, использующий стратегию преобразования.
+    /// </summary>
+    public class TextContext
+    {
+        private ITextTransformStrategy _strategy;
+
+        public TextContext(ITextTransformStrategy strategy)
+        {
+            _strategy = strategy;
+        }
+
+        public void SetStrategy(ITextTransformStrategy strategy)
+        {
+            _strategy = strategy;
+        }
+
+        public string ExecuteAlgorithm(string text)
+        {
+            return _strategy.TransformText(text);
+        }
+    }
+
+    #endregion
+
+    #region Фабричный метод
+
+    /// <summary>
+    /// Абстрактный класс-фабрика для создания стратегии преобразования текста.
+    /// </summary>
+    public abstract class TextTransformStrategyCreator
+    {
+        public abstract ITextTransformStrategy CreateStrategy(string transformationType, int textLength);
+    }
+
+    /// <summary>
+    /// Конкретная фабрика, создающая стратегии с дополнительной логикой:
+    /// Если длина текста превышает 100 символов, стратегия оборачивается в декоратор логирования.
+    /// </summary>
+    public class ConcreteTextTransformStrategyCreator : TextTransformStrategyCreator
+    {
+        public override ITextTransformStrategy CreateStrategy(string transformationType, int textLength)
+        {
+            // Выбираем базовую стратегию по типу
+            ITextTransformStrategy strategy = transformationType switch
+            {
+                "upper" => new TextToUpperStrategy(),
+                "lower" => new TextToLowerStrategy(),
+                "reverse" => new TextToReverseStrategy(),
+                _ => throw new ArgumentException("Неподдерживаемый тип преобразования")
+            };
+
+            // Если текст длинный, добавляем логирование
+            if (textLength > 100)
+            {
+                strategy = new LoggingTextTransformStrategyDecorator(strategy);
+            }
+            return strategy;
+        }
+    }
+
+    #endregion
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            // Запрос исходного текста
+            Console.WriteLine("Введите строку для преобразования:");
+            string input = Console.ReadLine();
+
+            // Запрос типа преобразования
+            Console.WriteLine("Выберите тип преобразования (upper, lower, reverse):");
+            string type = Console.ReadLine();
+
+            // Создаем фабрику, которая возьмет на себя решение, какую стратегию создать,
+            // с учетом дополнительного условия (длина текста)
+            TextTransformStrategyCreator creator = new ConcreteTextTransformStrategyCreator();
+            ITextTransformStrategy strategy = creator.CreateStrategy(type, input.Length);
+
+            // Передаем стратегию в контекст и выполняем преобразование
+            TextContext context = new TextContext(strategy);
+            string result = context.ExecuteAlgorithm(input);
+            Console.WriteLine("Результат: " + result);
+        }
     }
 }
